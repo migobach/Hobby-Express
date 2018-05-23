79fd93488bf7628e7bf54535ae4bef2daecfae65
Michael Bach <migobach@gmail.com>
2018-05-22 21:56:43 -0600

    add edit delete button

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var trailRouter = require('./routes/trail');
+var trail = require('./routes/trail');
+var methodOverride = require('method-override');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/trail', trailRouter);
+app.use('/trail', trail);
+app.use(methodOverride('_method'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/trail.js
+++ routes/trail.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Trail = require('../models').Trail;
 
+router.get('/', function(req, res) {
+  Trail.all(
+    {
+      order: [
+        ['createdAt', 'ASC']
+      ]
+    }
+  )
+    .then( function(trail) {
+      res.render('trail', { trail: trail})
+    })
+})
+
 // GET trail listings
 router.get('/', function(req, res) {
   Trail.all()
@@ -10,13 +23,42 @@ router.get('/', function(req, res) {
     })
 });
 
+//GET button
+router.get('/:id/edit', function(req, res) {
+  Trail.findById(req.params.id)
+  .then(function(trail) {
+    return res.render('edit', { trail: trail })
+  })
+})
+
+// PUT trail
+router.put('/:id', function(req, res) {
+  Trail.update(
+    { title: req.body.title },
+    { where: { id: req.params.id } }
+  )
+  .then( function () {
+    return res.redirect('/trail');
+  })
+});
+
 // POST add trail listing
 router.post('/', function(req, res) {
   var title = req.body.title;
   Trail.create({ title: title})
-    .then( function() {
-      res.redirect('/trail');
-    })
+  .then( function() {
+    res.redirect('/trail');
+  })
 })
 
+// DELETE trail
+router.delete('/:id', function(req, res) {
+  Trail.findById(req.params.id)
+    .then( function(trail) {
+      trail.destroy()
+    })
+    .then(function() {
+      return res.redirect('/trail');
+    })
+})
 module.exports = router;
